9454e5ffc9f0f0c9655910b4d5e138de0139f815
rehmeloz <renatamelo.pro@outlook.com>
2026-06-18 16:06:13 -0300

    adicona entidades na camada domain

---
+++ pro-atividade-app.Server/ProAtividade.Domain/Entities/Atividade.cs
@@ -0,0 +1,33 @@
+﻿namespace pro_atividade_app.Server.ProAtividade.Domain.Entities;
+
+public class Atividade
+{
+    public int Id { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public DateTime DataCriacao { get; set; }
+
+    public DateTime DataConclusao { get; set; }
+
+    public Prioridade Prioridade { get; set; }
+
+    public Atividade() => DataCriacao = DateTime.Now;
+
+    public Atividade(int id, string titulo, string descricao) : this()
+    { 
+       Id = id;
+       Titulo = titulo;
+       Descricao = descricao;
+    }
+
+    public void Concluir()
+    {
+        if (DataConclusao == null)
+            DataConclusao = DateTime.Now;
+        else
+            throw new Exception($"Atividade concluída em: {DataConclusao.ToString("dd/MM/yyyy HH:mm")}");
+    }
+}
---
+++ pro-atividade-app.Server/ProAtividade.Domain/Entities/Prioridade.cs
@@ -0,0 +1,12 @@
+﻿namespace pro_atividade_app.Server.ProAtividade.Domain.Entities;
+
+public enum Prioridade
+{
+    NaoDefinido,
+
+    Baixa,
+
+    Normal,
+
+    Alta
+}
