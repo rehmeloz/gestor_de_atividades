59172c27714c4fefc6d275bcaca6a612401eea18
rehmeloz <renatamelo.pro@outlook.com>
2026-06-18 16:06:39 -0300

    adiciona DataContext na camada de context

---
+++ pro-atividade-app.Server/ProAtividade.Data/Context/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pro_atividade_app.Server.ProAtividade.Domain.Entities;
+
+namespace pro_atividade_app.Server.ProAtividade.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Atividade> Atividades { get; set; }
+    }
+}
